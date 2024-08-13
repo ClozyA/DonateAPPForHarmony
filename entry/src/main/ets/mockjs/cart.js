96aeca2a88a480fc30ae6fa813d70e04aa354dd9
AoXuan <aoxuan233@gmail.com>
2024-08-13 16:05:09 +0800

    初步完成捐助岛

---
+++ entry/src/main/ets/mockjs/cart.js
@@ -0,0 +1,54 @@
+const cart = [
+    {
+
+        courseId: 1,
+        name: "城市无家可归者援助项目",
+        total_price: "5万",
+        used_price: "500",
+        start_time: "2024-02-01",
+        end_time: "2024-12-01",
+        company: "城市救助基金会",
+        isObject: true,
+        object_content: "提供临时住宿，日常饮食支持以及职业培训",
+        pic: $r("app.media.DonateSite1")
+    },
+    {
+        courseId: 2,
+        name: "社区文化振兴计划",
+        total_price: "8万",
+        used_price: "1600",
+        start_time: "2024-03-01",
+        end_time: "2025-02-01",
+        company: "社区文化发展中心",
+        isObject: true,
+        object_content: "建设社区图书馆，组织文化节，开展艺术工作坊",
+        pic: $r("app.media.DonateSite1")
+    },
+    {
+        courseId: 3,
+        name: "青少年体育活动支持",
+        total_price: "7万",
+        used_price: "1400",
+        start_time: "2024-04-01",
+        end_time: "2024-11-01",
+        company: "青少年体育基金会",
+        isObject: true,
+        object_content: "支持校园体育设施建设，举办校际运动会",
+        pic: $r("app.media.DonateSite1")
+    },
+    {
+        courseId: 4,
+        name: "社区环境改善计划",
+        total_price: "6万",
+        used_price: "1200",
+        start_time: "2024-05-01",
+        end_time: "2025-01-01",
+        company: "环境保护组织",
+        isObject: true,
+        object_content: "改善公园绿化，安装太阳能街灯，促进垃圾分类",
+        pic: $r("app.media.DonateSite1")
+    }
+]
+
+
+export default cart;
