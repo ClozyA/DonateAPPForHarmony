5e3925a90332ae105067433a071d6d65d4e3705e
AoXuan <aoxuan233@gmail.com>
2024-08-13 14:49:33 +0800

    实现导航栏的跳转、高亮显示当前所处位置

---
+++ entry/src/main/ets/mockjs/nav.js
@@ -0,0 +1,32 @@
+const nav = [
+    {
+        "icon": $r("app.media.planetoutline"),
+        "selectedIcon": $r("app.media.Heart"),
+        "mytxt": "首页",
+        "color": Color.Black,
+        "selectedColor": Color.Gray
+    },
+    {
+        "icon": $r("app.media.Donate3"),
+        "selectedIcon": $r("app.media.Heart"),
+        "mytxt": "捐助",
+        "color": Color.Black,
+        "selectedColor": Color.Gray
+    },
+    {
+        "icon": $r("app.media.ribbonoutline"),
+        "selectedIcon": $r("app.media.Heart"),
+        "mytxt": "捐助岛",
+        "color": Color.Black,
+        "selectedColor": Color.Gray
+    },
+    {
+        "icon": $r("app.media.personcircleoutline"),
+        "selectedIcon": $r("app.media.Heart"),
+        "mytxt": "我的",
+        "color": Color.Black,
+        "selectedColor": Color.Gray
+    }
+]
+
+export default nav
