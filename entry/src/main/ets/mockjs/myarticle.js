9135098d5deddc62e2d3c33011e5c72a80c5f8cc
AoXuan <aoxuan233@gmail.com>
2024-08-13 21:05:35 +0800

    完成导航栏跳转高亮当前页面、完成捐助岛的页面设计

---
+++ entry/src/main/ets/mockjs/myarticle.js
@@ -0,0 +1,22 @@
+const article = [
+    {
+        icon: $r("app.media.news3"),
+        mytxt: "探索未知的宇宙",
+        author: "张三",
+        recommend: 5
+    },
+    {
+        icon: $r("app.media.news3"),
+        mytxt: "科技改变生活",
+        author: "李四",
+        recommend: 4
+    },
+    {
+        icon: $r("app.media.news3"),
+        mytxt: "历史长河中的智慧",
+        author: "王五",
+        recommend: 3
+    }
+];
+
+export default article;
---
+++ entry/src/main/ets/mockjs/news_data.js
@@ -0,0 +1,19 @@
+const news = [
+    {
+        "icon": $r("app.media.news1"),
+        "title": "科技前沿动态",
+        "subtitle": "最新科技成果发布"
+    },
+    {
+        "icon": $r("app.media.sports1"),
+        "title": "体育赛事回顾",
+        "subtitle": "精彩瞬间不容错过"
+    },
+    {
+        "icon": $r("app.media.rescue1"),
+        "title": "灾害应对指南",
+        "subtitle": "安全自救小知识"
+    },
+];
+
+export default news;
